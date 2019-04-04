15b65e65d3e2a6b53c7f92a5424bb7db98d03911
Simone Franco <me@simonefranco.net>
2019-04-04 16:53:55 +0200

    Test commit

---
+++ WoAExplorer/CustomControls/StorageItemControl.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+namespace WoAExplorer.CustomControls
+{
+    public sealed partial class StorageItemControl : UserControl
+    {
+        public StorageItemControl()
+        {
+            InitializeComponent();
+        }
+
+        public IStorageItem Item
+        {
+            get { return (IStorageItem)GetValue(ItemProperty); }
+            set {
+                SetValue(ItemProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty ItemProperty = DependencyProperty.Register("Item", typeof(IStorageItem), typeof(StorageItemControl), new PropertyMetadata(default(int), new PropertyChangedCallback(async (s, e) =>
+            {
+                if (e.OldValue is IStorageItem oldValue) { }
+                if (e.NewValue is IStorageItem newValue)
+                {
+                    ((StorageItemControl)(s)).ItemNameLabel.Text = newValue.Name;
+                    ((StorageItemControl)(s)).ItemFlyoutName.Text = newValue.Name;
+                    if (newValue.IsOfType(StorageItemTypes.File))
+                    {
+                        StorageItemThumbnail icon = await GetFileIcon((StorageFile)newValue);
+                        BitmapImage bmp = new BitmapImage();
+                        bmp.SetSource(icon);
+                        ((StorageItemControl)(s)).ItemIcon.Source = bmp;
+                        ((StorageItemControl)(s)).ItemFlyoutIcon.Source = bmp;
+                        BasicProperties bp = await ((StorageFile)newValue).GetBasicPropertiesAsync();
+                        ((StorageItemControl)(s)).ItemFlyoutType.Text = ((StorageFile)newValue).DisplayType;
+                        ((StorageItemControl)(s)).ItemFlyoutSize.Text = bp.Size.ToString() + " bytes";
+                    }
+                    else if (newValue.IsOfType(StorageItemTypes.Folder))
+                    {
+                        StorageItemThumbnail icon = await GetFolderIcon((StorageFolder)newValue);
+                        BitmapImage bmp = new BitmapImage();
+                        bmp.SetSource(icon);
+                        ((StorageItemControl)(s)).ItemIcon.Source = bmp;
+                        ((StorageItemControl)(s)).ItemFlyoutIcon.Source = bmp;
+                    }
+                }
+            })));
+
+        private void MainGrid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (((IStorageItem)GetValue(ItemProperty)).IsOfType(StorageItemTypes.Folder)){
+                MainPage.loadFolderContentFromOutside(((IStorageItem)GetValue(ItemProperty)).Path);
+            }
+        }
+
+        public async static Task<StorageItemThumbnail> GetFileIcon(StorageFile file, uint size = 32)
+        {
+            StorageItemThumbnail iconTmb;
+            var imgExt = new[] { "bmp", "gif", "jpeg", "jpg", "png" }.FirstOrDefault(ext => file.Path.ToLower().EndsWith(ext));
+            if (imgExt != null)
+            {
+                var dummy = await ApplicationData.Current.TemporaryFolder.CreateFileAsync("dummy." + imgExt, CreationCollisionOption.ReplaceExisting); //may overwrite existing
+                iconTmb = await dummy.GetThumbnailAsync(ThumbnailMode.SingleItem, size);
+            }
+            else
+            {
+                iconTmb = await file.GetThumbnailAsync(ThumbnailMode.SingleItem, size);
+            }
+            return iconTmb;
+        }
+
+        public async static Task<StorageItemThumbnail> GetFolderIcon(StorageFolder folder, uint size = 32)
+        {
+            StorageItemThumbnail iconTmb;
+            iconTmb = await folder.GetThumbnailAsync(ThumbnailMode.SingleItem, size);
+            return iconTmb;
+        }
+    }
+}
---
+++ WoAExplorer/MainPage.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace WoAExplorer
+{
+
+    public sealed partial class MainPage : Page
+    {
+
+        public StorageFolder currentFolder;
+        public ObservableCollection<IStorageItem> currentItems = new ObservableCollection<IStorageItem>();
+        public ObservableCollection<string> currentItemsStrings = new ObservableCollection<string>();
+        public ObservableCollection<StorageFolder> currentFolders = new ObservableCollection<StorageFolder>();
+        public ObservableCollection<StorageFile> currentFiles = new ObservableCollection<StorageFile>();
+
+        public ObservableCollection<StorageFolder> availableDrives = new ObservableCollection<StorageFolder>();
+
+        public static MainPage thisPage = null;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            thisPage = this;
+            DataContext = this;
+            updateDrivesList();
+            loadFolderContent(SystemDataPaths.GetDefault().Windows);
+        }
+
+        public async void loadFolderContent(StorageFolder sf)
+        {
+            currentFolder = sf;
+            currentFolders.Clear();
+            currentFiles.Clear();
+            foreach(StorageFolder s in await sf.GetFoldersAsync())
+            {
+                currentFolders.Add(s);
+            }
+            foreach(StorageFile f in await sf.GetFilesAsync())
+            {
+                currentFiles.Add(f);
+            }
+            updatePage();
+        }
+
+        public static async void loadFolderContentFromOutside(string path)
+        {
+            thisPage.loadFolderContent(await StorageFolder.GetFolderFromPathAsync(path));
+        }
+
+        public async void loadFolderContent(string path)
+        {
+            loadFolderContent(await StorageFolder.GetFolderFromPathAsync(path));
+        }
+        
+        public async void updateDrivesList()
+        {
+            availableDrives.Clear();
+            char driveToScan = 'A';
+            for(; driveToScan <= 'Z'; driveToScan++)
+            {
+                try
+                {
+                    if (await StorageFolder.GetFolderFromPathAsync(driveToScan + ":\\") != null)
+                    {
+                        availableDrives.Add(await StorageFolder.GetFolderFromPathAsync(driveToScan + ":\\"));
+                        Debug.WriteLine(driveToScan);
+                    }
+                }catch(Exception e)
+                {
+                    Debug.WriteLine(driveToScan + " is not an available drive");
+                }
+            }
+        }
+
+        public void updatePage()
+        {
+            currentItemsStrings.Clear();
+            currentItems.Clear();
+            currentFolderPathTextBlock.Text = currentFolder.Path;
+            currentFolderPathTextBlock.Text = currentFolder.Path.Replace("\\", " > ");
+            foreach (StorageFolder s in currentFolders)
+            {
+                currentItemsStrings.Add(s.DisplayName);
+                currentItems.Add(s);
+            }
+            foreach (StorageFile f in currentFiles)
+            {
+                currentItemsStrings.Add(f.Name);
+                currentItems.Add(f);
+            }
+        }
+
+        private async void FolderUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(await currentFolder.GetParentAsync() == null)
+            {
+                return;
+            }
+            loadFolderContent(await currentFolder.GetParentAsync());
+        }
+    }
+}
